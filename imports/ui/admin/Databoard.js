d2f7fd675fcdde40e05206293ba955b3aa5ae53c
chile <levanchi74@gmail.com>
2019-07-08 23:52:04 +0700

    creating databoard page;

--- imports/ui/admin/Databoard.js
+++ imports/ui/admin/Databoard.js
@@ -43,21 +43,99 @@ class Databoard extends Component{
                     <div className="info-bar">
                         <h4 className="tab-name">Products</h4>
                         <div className="float-right">
-                            <img src="/profile/profile.jpg"/>
-                            <span>Chile</span>
-                                <div className="mail-noti">
-                                    <img src="/mail/mail.png"/>
-                                    <div className="Oval"
-                                </div>
-                                <div className="noti">
-                                    <img src ="/notification/notification.png"/>
-                                </div>
+                            <img className="img-adprofile" src="/profile/profile.jpg"/>
+                            
+                            <div className="account-option">
+                                <span>CHILE</span>
+                                <img src="/arrow-black/arrow-black.png"/>
+                                <ul>
+                                    <li>
+                                        <img src="/view-profile/profile.png"/>
+                                        <span>View profile</span>
+                                    </li>
+                                    <li>
+                                        <img src="/logout/logout.png"/>
+                                        <span>Logout</span>
+                                    </li>
+                                </ul>
+                            </div>
+
+                            <div className="notificationn">
+                                <img src="/mail/mail.png"/>
+                                <div className='noti-oval'>9+</div>
+                            </div>
+                            
+                            <div className="notificationn">
+                                <img src="/notification/notification.png"/>
+                                <div className='noti-oval'>9+</div>
+                            </div>
                                 
                         </div>
                     </div>
-                    <div className="row">
-                        <span>Products</span>
+                    <div className="function-bar">
+                        <div className="calendar-wrapper">
+                            <span>ORDERS DATE</span>
+                            <button className="btn-calendar">
+                                <span>01/08/2018 - 31/08/2018</span> 
+                                <img src="/calendar/calendar.png"/>
+                            </button>
+                            <button className="btn-calendar">
+                                <span>Today</span> 
+                            </button>
+                            <button className="btn-calendar">
+                                <span>Yesterday</span>      
+                            </button>
+                        </div>
+                        <div className="float-right">
+                            <div className="search-wrapper">
+                                <img src="/search/search.png"/>
+                                <input type="text" placeholder="Search order"/>
+                            </div>
+                            
+                            <button className="btn-export">
+                                <img src="/export-orange/export-orange.png"/>
+                                <span>Export</span>      
+                            </button>
+                           
+                        </div>
                     </div>
+
+                    
+                    <table className="table-wrapper">
+                        <tr>
+                            <th>ORDER ID</th>
+                            <th>ORDERED DATE</th>
+                            <th>DETAIL</th>
+                            <th>TOTAL($)</th>
+                            <th>STATUS</th>
+                        </tr>
+                        <tr>
+                            <td>AB12345</td>
+                            <td>Today,8th,2019</td>
+                            <td>Collete Stretch Linen Minidress (M) x 1</td>
+                            <td>60.00</td>
+                            <td>
+                                <button>Completed</button>
+                            </td>
+                            <td>
+                                <div className="action-option">
+                                    <span>Action</span>
+                                    <img src="/dropdown/dropdown.png"/>
+                                    {/* <ul>
+                                        <li>
+    
+                                            <span>Mark as Completed</span>
+                                        </li>
+                                        <li>
+                                            
+                                            <span>Mark as Canceled</span>
+                                        </li>
+                                    </ul> */}
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                   
                     
                 </div>
              
